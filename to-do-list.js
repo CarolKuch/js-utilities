c99dce90e19c190ef499560e9264991b48a45007
CarolKuch <kar.kucharska@gmail.com>
2021-02-24 10:25:40 +0100

    to-do-show-selected button functionality add

--- to-do-list.js
+++ to-do-list.js
@@ -1,9 +1,10 @@
 const addButton = document.querySelector('.to-do-add-button');
 let toDoUl = document.querySelector('.to-do-ul');
 let toDoText = document.querySelector('.to-do-text');
-let checkboxes = document.querySelectorAll('input[type=checkbox]');
+let checkboxes = [];
 let showSelectedToggler = document.querySelector('.to-do-show-selected');
 let selectedCheckboxes = [];
+let showToggler = 0;
 
 const addButtonToggler = () => {
     if (toDoText.value){
@@ -18,7 +19,7 @@ const createElement = () => {
     let text = document.createTextNode(toDoText.value);
     let checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
-    
+
     if(toDoText.value){
         li.appendChild(checkbox);
         li.appendChild(text);
@@ -27,16 +28,25 @@ const createElement = () => {
 }
 
 const showSelectedCheckboxes = (event) => {
-    if(event.target.checked){
+    
+    checkboxes = document.querySelectorAll('input[type=checkbox]'); 
+    console.log(checkboxes);
+    if(showToggler === 0){
+        if(event.target.checked){
+            checkboxes.forEach(checkbox => {
+                if(!checkbox.checked){   
+                    checkbox.disabled = true;
+                }
+            });
+        }
+        showToggler = 1;
+    }else{
         checkboxes.forEach(checkbox => {
-            if(checkbox.checked){   
-                selectedCheckboxes.push(checkbox);
-            }else{
-                checkbox.disabled = true;
-            }
-        });  
-        
+            checkbox.disabled = false;
+        });
+        showToggler = 0;
     }
+    console.log(showToggler);
 }
 
 addButton.addEventListener('click', createElement);
