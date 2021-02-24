7e9ca7d306168dbca328b922b1e3dceb0c660a68
CarolKuch <kar.kucharska@gmail.com>
2021-02-24 11:09:28 +0100

    Ability do delete to-do-list item

--- to-do-list.js
+++ to-do-list.js
@@ -2,9 +2,11 @@ const addButton = document.querySelector('.to-do-add-button');
 let toDoUl = document.querySelector('.to-do-ul');
 let toDoText = document.querySelector('.to-do-text');
 let checkboxes = [];
-let showSelectedToggler = document.querySelector('.to-do-show-selected');
+const showSelectedToggler = document.querySelector('.to-do-show-selected');
 let selectedCheckboxes = [];
 let showToggler = 0;
+let listItemDeleteButton = document.querySelector('.to-do-item-delete-button');
+
 
 const addButtonToggler = () => {
     if (toDoText.value){
@@ -14,35 +16,42 @@ const addButtonToggler = () => {
     }
 }
 
-const createElement = () => {
-    let li = document.createElement('li');
-    let text = document.createTextNode(toDoText.value);
-    let checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-
+const createElement = () => {    
     if(toDoText.value){
+        let li = document.createElement('li');
+        let text = document.createTextNode(toDoText.value);
+        let checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        let x = document.createElement('img');
+        x.setAttribute('src', 'img/x.svg');
+        x.setAttribute('class', 'to-do-item-delete-button');
         li.appendChild(checkbox);
         li.appendChild(text);
+        li.appendChild(x);
         toDoUl.appendChild(li);
+        x.addEventListener('click', deleteListItem, false);
     }
 }
 
-const showSelectedCheckboxes = (event) => {
-    
+const deleteListItem = (e) => {
+    e.target.parentElement.remove();
+}
+
+const showSelectedCheckboxes = (e) => {    
     checkboxes = document.querySelectorAll('input[type=checkbox]'); 
     console.log(checkboxes);
     if(showToggler === 0){
-        if(event.target.checked){
+        if(e.target.checked){
             checkboxes.forEach(checkbox => {
                 if(!checkbox.checked){   
-                    checkbox.disabled = true;
+                    checkbox.parentElement.style.display = "none";
                 }
             });
         }
         showToggler = 1;
     }else{
         checkboxes.forEach(checkbox => {
-            checkbox.disabled = false;
+            checkbox.parentElement.style.display = "block";
         });
         showToggler = 0;
     }
@@ -51,4 +60,5 @@ const showSelectedCheckboxes = (event) => {
 
 addButton.addEventListener('click', createElement);
 toDoText.addEventListener('keyup', addButtonToggler);
-showSelectedToggler.addEventListener('change', ()=>{showSelectedCheckboxes(event)});
+showSelectedToggler.addEventListener('change', showSelectedCheckboxes, false);
+listItemDeleteButton.addEventListener('click', deleteListItem, false);
